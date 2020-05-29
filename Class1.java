9d52f0e7d6255878b29300bd3393ed90efe314dc
mantuk87 <mantu.montu1987@gmail.com>
2020-05-29 12:42:35 +0530

    Second commit for master - added new SOP

--- Class1.java
+++ Class1.java
@@ -9,7 +9,8 @@ public class Class1
     @Test
     public void shouldAnswerWithTrue()
     {
-	System.out.println("SOP from class 1");
+	System.out.println("SOP 1 from class 1");
+	System.out.println("SOP 2 from class 1");
 
     }
 }
