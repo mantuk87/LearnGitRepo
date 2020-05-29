e667a938fdd932f4b6007de5fabed3e3efb29c63
mantuk87 <mantu.montu1987@gmail.com>
2020-05-29 12:41:37 +0530

    Initial Commit

---
+++ Class1.java
@@ -0,0 +1,15 @@
+package com.newWorkspace.qa.Login;
+
+import org.testng.annotations.Test;
+
+
+public class Class1
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+	System.out.println("SOP from class 1");
+
+    }
+}
